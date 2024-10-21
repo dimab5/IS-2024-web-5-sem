ebdb2003d89eec4b777f891ecdea231f2794697e
Борисов Дмитрий Юрьевич <113174105+dimab5@users.noreply.github.com>
2024-10-21 13:39:44 +0300

    Delete labs directory

--- labs/js/activeMenu.js
+++
@@ -1,9 +0,0 @@
-const currentPage = window.location.pathname.split('/').pop();
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll("nav ul li a").forEach(link => { console.log(link);
-        if (link.getAttribute('href') === currentPage) {
-            link.classList.add("active");
-        }
-    });
-});
